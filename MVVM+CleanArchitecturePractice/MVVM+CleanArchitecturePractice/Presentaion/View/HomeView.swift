cef23d500c6c1e78e9c5e8167f080ecb8b1c25f5
Masayuki Kawahima <poppinmasa@gmail.com>
2026-07-23 14:52:22 +0900

    feat: ホーム画面の仮レイアウト作成

--- MVVM+CleanArchitecturePractice/MVVM+CleanArchitecturePractice/Presentaion/View/HomeView.swift
+++ MVVM+CleanArchitecturePractice/MVVM+CleanArchitecturePractice/Presentaion/View/HomeView.swift
@@ -9,33 +9,44 @@ import UIKit
 
 class HomeView: UIView {
 
-    // 画面中央に表示する "Hello World" ラベル
-    private let helloLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Hello World"
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+  let searchBar = UISearchBar()
+  let tableView = UITableView()
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupView()
-    }
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .systemBackground
+    setupSearchBar()
+    setupTableView()
+  }
 
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupView()
-    }
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
-    // ラベルの追加と中央配置の制約設定
-    private func setupView() {
-        backgroundColor = .systemBackground
-        addSubview(helloLabel)
+  private func setupSearchBar() {
+    searchBar.placeholder = "リポジトリを検索"
+    searchBar.searchBarStyle = .minimal
+    searchBar.showsCancelButton = true
+    searchBar.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(searchBar)
 
-        NSLayoutConstraint.activate([
-            helloLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            helloLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ])
-    }
+    NSLayoutConstraint.activate([
+      searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+      searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+      searchBar.trailingAnchor.constraint(equalTo: trailingAnchor)
+    ])
+  }
+
+  private func setupTableView() {
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(tableView)
+
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+      tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+    ])
+  }
 }
--- MVVM+CleanArchitecturePractice/MVVM+CleanArchitecturePractice/Presentaion/ViewController/HomeViewController.swift
+++ MVVM+CleanArchitecturePractice/MVVM+CleanArchitecturePractice/Presentaion/ViewController/HomeViewController.swift
@@ -9,25 +9,56 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+  // HomeView をプロパティとして保持
+  private let homeView = HomeView()
+
+  // テーブルに表示するリポジトリ一覧（後でモデル型に差し替え予定）
+  private var repositories: [String] = [
+    "apple/swift",
+    "tensorflow/tensorflow",
+    "facebook/react",
+    "microsoft/vscode",
+    "torvalds/linux"
+  ]
+
   override func loadView() {
-    view = HomeView()
+    view = homeView
   }
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
+    // searchBar のデリゲート接続
+    homeView.searchBar.delegate = self
+
+    // tableView のデリゲート/データソース接続
+    homeView.tableView.dataSource = self
+    homeView.tableView.delegate = self
+  }
+}
+
+// MARK: - UISearchBarDelegate
+extension HomeViewController: UISearchBarDelegate {
+  func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.text = ""              // 入力中の文字を空にする
+    searchBar.resignFirstResponder() // キーボードを閉じる（表示中の場合）
+  }
+}
+
+// MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return repositories.count
   }
 
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = repositories[indexPath.row]
+    return cell
+  }
+}
 
-  /*
-   // MARK: - Navigation
-
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
-
+// MARK: - UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+  // 行選択時などの処理は後で実装
 }
